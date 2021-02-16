6d39d41fb0f0ade94252dd902acc9d0163aea212
Ben Stockil <benstockil@outlook.com>
2021-02-16 15:34:08 +0000

    JS update

--- js/parser.js
+++ js/parser.js
@@ -1,8 +1,29 @@
 fetch("https://l5info.live/info.json")
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log("AAAAAAAAAAAA")
-  });
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((err) => {
+        console.log("AAAAAAAAAAAA")
+    });
+
+let page = document.getElementById("page");
+data.sections.forEach(section => {
+    page.innerHTML += `
+        <div class=section>
+            <h2>${section.title}</h2>
+            <ul>
+    `;
+
+    section.items.forEach(item => {
+        page.innerHTML += `
+                <li>${item}</li>
+        `
+    });
+
+    page.innerHTML += `
+            </ul>
+        </div>
+    `
+    
+    });
