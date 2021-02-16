22c4498515cfd4760e1e06e2e8b5c73daeeca8bb
Ben Stockil <benstockil@outlook.com>
2021-02-16 15:56:03 +0000

    finished(?) parser

--- js/parser.js
+++ js/parser.js
@@ -1,28 +1,58 @@
 fetch("https://l5info.live/info.json")
     .then((response) => response.json())
     .then((data) => {
-        console.log(data);
+        let page = document.getElementById("page");
+        document.getElementById("title").innerText = data.title;
+        data.sections.forEach(section => {
+
+            page.innerHTML += `
+                <div class=section>
+                <h2>${section.title}</h2>
+                <ul>
+            `;
+
+            section.items.forEach(item => {
+                if(typeof item == "object") {
+                    page.innerHTML += `
+                        <li><a href=${item.url}>${item.text}</a></li>
+                    `   
+                }
+                else {
+                    page.innerHTML += `
+                        <li>${item}</li>
+                    `
+                }
+            });
+
+            page.innerHTML += `
+                </ul>
+                </div>
+            `
+            
+        });
     })
     .catch((err) => {
-        console.log("AAAAAAAAAAAA")
+        console.log(err)
     });
 
+console.log(info);
+
 let page = document.getElementById("page");
-data.sections.forEach(section => {
+info.sections.forEach(section => {
     page.innerHTML += `
         <div class=section>
-            <h2>${section.title}</h2>
-            <ul>
+        <h2>${section.title}</h2>
+        <ul>
     `;
 
     section.items.forEach(item => {
         page.innerHTML += `
-                <li>${item}</li>
+            <li>${item}</li>
         `
     });
 
     page.innerHTML += `
-            </ul>
+        </ul>
         </div>
     `
     
