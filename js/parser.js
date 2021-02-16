132a28126da705a0773eacc28148fc099b2c6f4f
Ben Stockil <benstockil@outlook.com>
2021-02-16 16:11:48 +0000

    fixed some issues, reformatted js

--- js/parser.js
+++ js/parser.js
@@ -5,55 +5,17 @@ fetch("https://l5info.live/info.json")
         document.getElementById("title").innerText = data.title;
         data.sections.forEach(section => {
 
-            page.innerHTML += `
-                <div class=section>
-                <h2>${section.title}</h2>
-                <ul>
-            `;
-
+            let sectionHTML = "";
+            sectionHTML += `<div class=section><h2>${section.title}</h2><ul>`;
             section.items.forEach(item => {
-                if(typeof item == "object") {
-                    page.innerHTML += `
-                        <li><a href=${item.url}>${item.text}</a></li>
-                    `   
-                }
-                else {
-                    page.innerHTML += `
-                        <li>${item}</li>
-                    `
-                }
+                if(typeof item == "object") sectionHTML += `<li><a href=${item.url}>${item.text}</a></li>`;
+                else sectionHTML += `<li>${item}</li>`;
             });
-
-            page.innerHTML += `
-                </ul>
-                </div>
-            `
+            sectionHTML += `</ul></div>`
+            page.innerHTML += sectionHTML;
             
         });
     })
     .catch((err) => {
         console.log(err)
-    });
-
-console.log(info);
-
-let page = document.getElementById("page");
-info.sections.forEach(section => {
-    page.innerHTML += `
-        <div class=section>
-        <h2>${section.title}</h2>
-        <ul>
-    `;
-
-    section.items.forEach(item => {
-        page.innerHTML += `
-            <li>${item}</li>
-        `
-    });
-
-    page.innerHTML += `
-        </ul>
-        </div>
-    `
-    
     });
